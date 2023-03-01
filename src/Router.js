5623ce08fe420d5173a09e63353c290382c75adf
Kerem Mert <kerem37mert@yahoo.com>
2023-03-01 17:45:42 +0300

    first commit

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+const Router = () => {
+  return(
+    <SafeAreaView>
+      <Text>Kerem</Text>
+      <Text>dfdsf</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Router;
