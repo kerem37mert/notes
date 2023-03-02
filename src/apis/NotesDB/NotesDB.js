b80b637385a56c93acd142ba1a69003ab5e9b6a7
Kerem Mert <kerem37mert@yahoo.com>
2023-03-02 20:45:57 +0300

    sqlite config

---
+++ src/apis/NotesDB/NotesDB.js
@@ -0,0 +1,40 @@
+import { openDatabase } from "react-native-sqlite-storage";
+
+const db = openDatabase({
+    name: "my",
+});
+
+const createNotes = () => {
+    db.transaction(txn => {
+        txn.executeSql(
+            'CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(100), text TEXT, date DATETIME DEFAULT CURRENT_TIMESTAMP)',
+            [],
+            (sqlTxn, res) => {
+                console.log("table created successfulyy");
+            },
+            error => {
+                console.log(error.message);
+            },
+        )
+    });
+}
+
+const addNote = (title, text) => {
+    db.transaction(txn => {
+        txn.executeSql(
+            'INSERT INTO notes (title, text) VALUES(?,?) ',
+            [title, text],
+            (sqlTxn, res) => {
+                console.log("notes added successfully");
+            },
+            error => {
+                console.log(error.message);
+            }
+        )
+    });
+}
+
+export {
+    createNotes,
+    addNote,
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,71 @@
-import React, { useState } from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, Button } from "react-native";
 import styles from "./Home.style";
 import Note from "../../components/Note";
 import MasonryList from '@react-native-seoul/masonry-list';
+import { openDatabase } from "react-native-sqlite-storage";
+import { createNotes, addNote } from "../../apis/NotesDB/NotesDB";
 
 const Home = () => {
 
+    const [notes, setNotes] = useState([]);
+
+    const db = openDatabase({
+        name: "my",
+    });
+    
+    
+    const getNotes = () => {
+        db.transaction(txn => {
+            txn.executeSql(
+                "SELECT * FROM notes order by date DESC",
+                [],
+                (sqlTxn, res)=> {
+                    console.log("notes retrieved successfully");
+                    let len = res.rows.length;
+
+                    if(len > 0)
+                    {
+                        let result = [];
+                        for(let i=0; i<len; i++)
+                        {
+                            let item = res.rows.item(i);
+                            result.push({id: item.id, title: item.title, text: item.text, date: item.date});
+                        }
+
+                        setNotes(result);
+                    }
+                },
+                error => {
+                    console.log(error.message);
+                }
+            );
+        });
+    }
+
+    useEffect(() => {
+            createNotes();
+            getNotes();
+    }, []);
+    
+
     const renderNote = ({item}) => {
         return(
-            <Note text={item} />
+            <Note text={item.title} />
         );
     }
 
     return(
         <SafeAreaView style={styles.container}>
             <MasonryList
-                 data={[1,2,3,4,5,6]}
-                 numColumns={2}
-                 showsVerticalScrollIndicator={false}
-                 renderItem={renderNote}
-                 refreshing={false}
+                data={notes}
+                numColumns={2}
+                showsVerticalScrollIndicator={false}
+                renderItem={renderNote}
+                refreshing={false}
             />
+            <Button title="click" onPress={() => {addNote("dsfhjsdf","şdıfjhdg"); getNotes()}} />
         </SafeAreaView>
     );
 }
-
 export default Home;
