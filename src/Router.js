9400d37efd9dc8124c90598857d7eb173275385f
Kerem Mert <kerem37mert@yahoo.com>
2023-03-03 11:16:04 +0300

    Details page start

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./pages/Home";
+import Details from "./pages/Details";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const Router = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="HomePage" component={Home} />
+        <Stack.Screen name="DetailsPage" component={Detail} />
       </Stack.Navigator> 
     </NavigationContainer>  
   );
---
+++ src/pages/Details/Details.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native";
+import styles from "./Details.styles";
+
+const Details = ({navigation, route}) => {
+    return(
+        <SafeAreaView style={styles.container}>
+            
+        </SafeAreaView>
+    );
+}
+
+export default Details;
