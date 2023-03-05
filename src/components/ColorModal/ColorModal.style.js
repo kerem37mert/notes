ba8c4bafd4ec06bbb09560dd1dffb2fa23818772
Kerem Mert <kerem37mert@yahoo.com>
2023-03-05 21:42:11 +0300

    color modal detail

--- src/components/ColorModal/ColorModal.style.js
+++ src/components/ColorModal/ColorModal.style.js
@@ -2,6 +2,25 @@ import { StyleSheet, Dimensions } from "react-native";
 
 export default StyleSheet.create({
     container: {
-        backgroundColor: "red"
+        position: "absolute",
+        bottom: 0,
+        width: Dimensions.get("window").width,
+        padding: 20,
+    
+        
+    },
+    title: {
+        color: "#000",
+        fontSize: 16,
+        marginBottom: 20
+    },
+    bgColor: {
+        height: 24,
+        width: 24,
+        borderRadius: 12,
+        borderWidth: 2,
+        borderColor: "#000",
+        marginLeft: 10,
+        marginRight: 10
     }
 });
